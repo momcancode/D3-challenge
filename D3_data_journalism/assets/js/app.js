4f8ab5d678af089e7919179d0727ad713269eede
Tracey Phuong Ha <thao.ph.ha@gmail.com>
2020-09-10 16:26:08 +1000

    Create axes labels

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -76,53 +76,23 @@ d3.csv("./assets/data/data.csv").then(function(censusData) {
 	.attr("dy", "4")
 	.text(d => d.abbr)
 	.attr("class", "stateText");
+
+	// Create axes labels
+	chartGroup.append("text")
+	.attr("transform", "rotate(-90)")
+	.attr("y", 0 - margin.left + 40)
+	.attr("x", 0 - (height / 2))
+	.attr("dy", "1em")
+	.attr("class", "aText")
+	.text("Lacks Healthcare (%)");
+
+	chartGroup.append("text")
+	.attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
+	.attr("class", "aText")
+	.text("In Poverty (%)");
+
+}).catch(function(error) {
+	console.log(error);
 })
-
-
-
-
-
-
-
-
-// 	// Step 6: Initialize tool tip
-// 	// ==============================
-// 	var toolTip = d3.tip()
-// 		.attr("class", "tooltip")
-// 		.offset([80, -60])
-// 		.html(function(d) {
-// 			return (`${d.rockband}<br>Hair length: ${d.hair_length}<br>Hits: ${d.num_hits}`);
-// 		});
-
-// 	// Step 7: Create tooltip in the chart
-// 	// ==============================
-// 	chartGroup.call(toolTip);
-
-// 	// Step 8: Create event listeners to display and hide the tooltip
-// 	// ==============================
-// 	circlesGroup.on("click", function(data) {
-// 		toolTip.show(data, this);
-// 	})
-// 		// onmouseout event
-// 		.on("mouseout", function(data, index) {
-// 			toolTip.hide(data);
-// 		});
-
-// 	// Create axes labels
-// 	chartGroup.append("text")
-// 		.attr("transform", "rotate(-90)")
-// 		.attr("y", 0 - margin.left + 40)
-// 		.attr("x", 0 - (height / 2))
-// 		.attr("dy", "1em")
-// 		.attr("class", "axisText")
-// 		.text("Number of Billboard 100 Hits");
-
-// 	chartGroup.append("text")
-// 		.attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
-// 		.attr("class", "axisText")
-// 		.text("Hair Metal Band Hair Length (inches)");
-// }).catch(function(error) {
-// 	console.log(error);
-// }
 
 
